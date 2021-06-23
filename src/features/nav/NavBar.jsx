df5e16b5fa14ca4ba0fe682110cd0650cb5a3d19
emilywilbur <ewilbur94@gmail.com>
2021-06-23 10:06:15 -0400

    sign in/sign out

--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,27 +1,37 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { Button, Container, Menu } from 'semantic-ui-react';
-import SignedInMenu from './SignedInMenu';
-import SignedOutMenu from './SignedOutMenu';
+import React, { useState } from "react";
+import { NavLink, useHistory } from "react-router-dom";
+import { Button, Container, Menu } from "semantic-ui-react";
+import SignedInMenu from "./SignedInMenu";
+import SignedOutMenu from "./SignedOutMenu";
 
+export default function NavBar({ setFormOpen }) {
+  const history = useHistory();
+  const [authenticated, setAuthenticated] = useState(false);
 
-export default function NavBar({setFormOpen}) {
-    return (
-        <Menu inverted fixed='top'>
-            <Container>
-                <Menu.Item as={NavLink} exact to='/' header>
-                    <img src="/assets/logo.png" alt='logo' style={{marginRight: 15}}/>
-                    EventSpace
-                </Menu.Item>
-                <Menu.Item as={NavLink} to='/events' name="Events" />
-                <Menu.Item as={NavLink} to='/createEvent'>
-                    <Button positive inverted content='Create Event'/>
-                </Menu.Item>
-                <SignedOutMenu />
-                <SignedInMenu />
-                
-            </Container>
+  function handleSignOut() {
+      setAuthenticated(false);
+      history.push('/');
+  }
 
-        </Menu>
-    )
+  return (
+    <Menu inverted fixed="top">
+      <Container>
+        <Menu.Item as={NavLink} exact to="/" header>
+          <img src="/assets/logo.png" alt="logo" style={{ marginRight: 15 }} />
+          EventSpace
+        </Menu.Item>
+        <Menu.Item as={NavLink} to="/events" name="Events" />
+        {authenticated && (
+          <Menu.Item as={NavLink} to="/createEvent">
+            <Button positive inverted content="Create Event" />
+          </Menu.Item>
+        )}
+        {authenticated ? (
+          <SignedInMenu signOut={handleSignOut} />
+        ) : (
+          <SignedOutMenu setAuthenticated={setAuthenticated} />
+        )}
+      </Container>
+    </Menu>
+  );
 }
--- src/features/nav/SignedInMenu.jsx
+++ src/features/nav/SignedInMenu.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Menu, Image, Dropdown } from "semantic-ui-react";
 
-export default function SignedInMenu() {
+export default function SignedInMenu({signOut}) {
   return (
     <Menu.Item position="right">
       <Image avatar spaced='right' src='/assets/user.png' />
@@ -10,7 +10,7 @@ export default function SignedInMenu() {
       <Dropdown.Menu>
           <Dropdown.Item as={Link} to='/createEvent' text='Create Event' icon='plus' />
           <Dropdown.Item text='My Profile' icon='user' />
-          <Dropdown.Item text='Sign Out' icon='power' />
+          <Dropdown.Item onClick={signOut} text='Sign Out' icon='power' />
       </Dropdown.Menu>
       </Dropdown>
     </Menu.Item>
