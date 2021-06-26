21739701c2634c798b37fac037fe47cd39f81e71
emilywilbur <ewilbur94@gmail.com>
2021-06-26 10:36:06 -0400

    redux setup

--- src/index.js
+++ src/index.js
@@ -1,30 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css';
-import './app/layout/styles.css';
-import App from './app/layout/App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "semantic-ui-css/semantic.min.css";
+import "./app/layout/styles.css";
+import App from "./app/layout/App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "./app/store/configureStore";
 
+const store = configureStore();
 
-const rootEl = document.getElementById('root');
+const rootEl = document.getElementById("root");
 
 function render() {
   ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+
     rootEl
-)};
+  );
+}
 
 if (module.hot) {
-  module.hot.accept('./app/layout/App', function() {
+  module.hot.accept("./app/layout/App", function () {
     setTimeout(render);
   });
 }
 
 render();
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
