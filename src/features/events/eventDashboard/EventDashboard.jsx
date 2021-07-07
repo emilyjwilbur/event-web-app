baf39818b1a774811d1c3a98055d22a6f8a48915
emilywilbur <ewilbur94@gmail.com>
2021-07-07 13:03:34 -0400

    added a useFirestoreDoc hook

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,35 +1,35 @@
-import React, {useEffect} from "react";
+import React from "react";
 import { Grid } from "semantic-ui-react";
 import EventList from "./EventList";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import EventListItemPlaceholder from "./EventListItemPlaceholder";
 import EventFilters from "./EventFilters";
-import { getEventsFromFirestore } from "../../../app/firestore/firestoreService";
+import { listenToEventsFromFirestore } from '../../../app/firestore/firestoreService';
+
+import { listenToEvents } from "../eventActions";
+
+import useFirestoreCollection from "../../../app/hooks/useFirestoreCollection";
 
 export default function EventDashboard() {
-  const {events} = useSelector(state => state.event);
-  const {loading} = useSelector(state => state.async);
+  const dispatch = useDispatch();
+  const { events } = useSelector((state) => state.event);
+  const { loading } = useSelector((state) => state.async);
 
-  useEffect(() => {
-    const unsubscribe = getEventsFromFirestore({
-      next: snapshot => console.log(snapshot.docs.map(docSnapshot => docSnapshot.data())),
-      error: error => console.log(error)
-    })
-    return unsubscribe
-  })
-
-
-
+useFirestoreCollection({
+  query: () => listenToEventsFromFirestore(),
+  data: events => dispatch(listenToEvents(events)),
+  deps: [dispatch]
+})
 
   return (
     <Grid>
       <Grid.Column width={10}>
-        {loading && 
+        {loading && (
           <>
-          <EventListItemPlaceholder />
-          <EventListItemPlaceholder />
+            <EventListItemPlaceholder />
+            <EventListItemPlaceholder />
           </>
-        }
+        )}
         <EventList events={events} />
       </Grid.Column>
       <Grid.Column width={6}>
