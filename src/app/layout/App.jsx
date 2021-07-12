d78ed56b3def3266fabd37faf84d2ca3c6f34efa
emilywilbur <ewilbur94@gmail.com>
2021-07-12 16:28:36 -0400

    added profile page

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -13,6 +13,7 @@ import ErrorComponent from "../common/errors/ErrorComponent";
 import AccountPage from "../../features/auth/AccountPage";
 import { useSelector } from "react-redux";
 import LoadingComponent from "./LoadingComponent";
+import ProfilePage from "../../features/profiles/profilePage/ProfilePage";
 
 export default function App() {
   const {key} = useLocation();
@@ -36,6 +37,7 @@ export default function App() {
               <Route path="/events/:id" component={EventDetailedPage} />
               <Route path={["/createEvent", "/manage/:id"]} component={EventForm} key={key} />
               <Route path="/account" component={AccountPage} />
+              <Route path="/profile/:id" component={ProfilePage} />
               <Route path="/error" component={ErrorComponent} />
             </Container>
           </>
--- src/features/nav/SignedInMenu.jsx
+++ src/features/nav/SignedInMenu.jsx
@@ -27,7 +27,7 @@ export default function SignedInMenu() {
       <Dropdown pointing='top left' text={currentUser.displayName} >
       <Dropdown.Menu>
           <Dropdown.Item as={Link} to='/createEvent' text='Create Event' icon='plus' />
-          <Dropdown.Item text='My Profile' icon='user' />
+          <Dropdown.Item as={Link} to={`/profile/${currentUser.uid}`} text='My Profile' icon='user' />
           <Dropdown.Item as={Link} to='/account' text='My Account' icon='settings' />
           <Dropdown.Item 
           onClick={handleSignOut}
