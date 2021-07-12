76e6e20e4b081ecd9b113fcc15bb5776bd1711c3
emilywilbur <ewilbur94@gmail.com>
2021-07-12 18:15:49 -0400

    added the update user actions

--- src/features/profiles/profilePage/ProfileHeader.jsx
+++ src/features/profiles/profilePage/ProfileHeader.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Button, Divider, Grid, Header, Item, Reveal, Segment, Statistic } from 'semantic-ui-react';
 
-export default function ProfileHeader() {
+export default function ProfileHeader({profile}) {
     return (
         <Segment>
         <Grid>
             <Grid.Column width={12}>
                 <Item.Group>
                     <Item>
-                        <Item.Image avatar size='small' src='/assets/user.png' />
+                        <Item.Image avatar size='small' src= {profile.photoURL || '/assets/user.png'} />
                         <Item.Content verticalAlign='middle' >
                             <Header as='h1' style={{display: 'block', marginBottom: 10}}
-                            content='Display Name' />
+                            content={profile.displayName} />
                         </Item.Content>
                     </Item>
                 </Item.Group>
