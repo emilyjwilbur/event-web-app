0a77d5d800163dd4c5b571f615203997fcbe38d4
emilywilbur <ewilbur94@gmail.com>
2021-07-13 12:26:51 -0400

    delete photo functionality

--- src/features/profiles/profilePage/ProfileContent.jsx
+++ src/features/profiles/profilePage/ProfileContent.jsx
@@ -16,7 +16,7 @@ export default function ProfileContent({profile, isCurrentUser}) {
         menu={{fluid: true, vertical: true}}
         menuPosition='right'
         panes={panes}
-        activeIndex={1}
+       
         />
     )
 }
