e99f2738d7bd82eef765e3acea7323c5645f9ad6
emilywilbur <ewilbur94@gmail.com>
2021-07-13 11:04:15 -0400

    displaying images

--- src/features/profiles/profilePage/PhotosTab.jsx
+++ src/features/profiles/profilePage/PhotosTab.jsx
@@ -1,40 +1,52 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Button, Card, Grid, Header, Tab, Image } from "semantic-ui-react";
 import PhotoUploadWidget from "../../../app/common/photos/PhotoUploadWidget";
-
+import { getUserPhotos } from "../../../app/firestore/firestoreService";
+import useFirestoreCollection from "../../../app/hooks/useFirestoreCollection";
+import { listenToUserPhotos } from "../profileActions";
 
 export default function PhotosTab({ profile, isCurrentUser }) {
-  const [editMode, setEditMode] = useState(true);
+  const dispatch = useDispatch();
+  const [editMode, setEditMode] = useState(false);
+  const { loading } = useSelector((state) => state.async);
+  const { photos } = useSelector((state) => state.profile);
+
+  useFirestoreCollection({
+    query: () => getUserPhotos(profile.id),
+    data: (photos) => dispatch(listenToUserPhotos(photos)),
+    deps: [profile.id, dispatch],
+  });
+
   return (
-    <Tab.Pane>
+    <Tab.Pane loading={loading}>
       <Grid>
         <Grid.Column width={16}>
-          <Header
-            floated="left"
-            icon="user"
-            content={`Photos`}
-          />
-          {isCurrentUser &&
-          <Button
-            onClick={() => setEditMode(!editMode)}
-            floated="right"
-            basic
-            content={editMode ? "Cancel" : "Add Photo"}
-          />}
+          <Header floated="left" icon="user" content={`Photos`} />
+          {isCurrentUser && (
+            <Button
+              onClick={() => setEditMode(!editMode)}
+              floated="right"
+              basic
+              content={editMode ? "Cancel" : "Add Photo"}
+            />
+          )}
         </Grid.Column>
         <Grid.Column width={16}>
           {editMode ? (
-           <PhotoUploadWidget setEditMode={setEditMode} />
+            <PhotoUploadWidget setEditMode={setEditMode} />
           ) : (
-           <Card.Group itemsPerRow={5}>
-               <Card>
-                   <Image src='/assets/user.png' />
-                   <Button.Group fluid widths={2}>
-                       <Button basic color='green' content='Main' />
-                       <Button basic color='red' icon='trash' />
-                   </Button.Group>
-               </Card>
-           </Card.Group>
+            <Card.Group itemsPerRow={5}>
+              {photos.map((photo) => (
+                <Card kay={photo.id}>
+                  <Image src={photo.url} />
+                  <Button.Group fluid widths={2}>
+                    <Button basic color="green" content="Main" />
+                    <Button basic color="red" icon="trash" />
+                  </Button.Group>
+                </Card>
+              ))}
+            </Card.Group>
           )}
         </Grid.Column>
       </Grid>
